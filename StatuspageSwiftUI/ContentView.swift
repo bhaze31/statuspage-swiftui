a24a7f3727d3d76071e6f60fdb66608a0ca8fa5f
Brian Hasenstab <brian.hasenstab31@gmail.com>
2021-03-24 08:53:51 -0400

    Initial Commit

---
+++ StatuspageSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatuspageSwiftUI
+//
+//  Created by Brian Hasenstab on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatuspageSwiftUI/StatuspageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatuspageSwiftUIApp.swift
+//  StatuspageSwiftUI
+//
+//  Created by Brian Hasenstab on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct StatuspageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
